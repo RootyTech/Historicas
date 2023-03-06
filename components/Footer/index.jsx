5e2ba5bfb2eae0e7a6a03b3d5077050c9d954a78
Juan Galvis <juangalvis.sh@gmail.com>
2023-03-05 23:50:30 -0500

    Scroll to top (smooth)

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,31 +1,30 @@
-import Image from 'next/image'
-import Logo from '../../assets/Rooty.png';
-import Top from '../../assets/top.png';
-import styles from '../../styles/footer.module.css'
+import Image from "next/image";
+import Logo from "../../assets/Rooty.png";
+import Top from "../../assets/top.png";
+import styles from "../../styles/footer.module.css";
 
 export const Footer = () => {
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <footer className={styles.footer}>
-        <a href="#top">
-            <Image
-                src={Top}
-                alt="To top"
-                width={40}
-                height={40}
-            />
-        </a>
-        <span>
-            Con esta app no se quiere incomodar a ninguna de las mujeres aquí compartidas. 
-            Solo es una iniciativa para darle visibilidad a su rol en la sociedad 😊
-        </span>
-        <h3>Hecho con 💜</h3>
-        
-        <Image
-            src={Logo}
-            alt="Rooty logo"
-            width={40}
-            height={40}
-        />
+      <Image
+        src={Top}
+        alt="To top"
+        width={40}
+        height={40}
+        onClick={scrollTop}
+      />
+      <span>
+        Con esta app no se quiere incomodar a ninguna de las mujeres aquí
+        compartidas. Solo es una iniciativa para darle visibilidad a su rol en
+        la sociedad 😊
+      </span>
+      <h3>Hecho con 💜</h3>
+
+      <Image src={Logo} alt="Rooty logo" width={40} height={40} />
     </footer>
   );
-}
+};
