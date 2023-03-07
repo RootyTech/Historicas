46cb447e1a3fba618b3cdb32ea12cb09fd7efdc0
Juan Galvis <juangalvis.sh@gmail.com>
2023-03-06 19:32:09 -0500

    PWA

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+        <meta name="theme-color" content='#6327BD' />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
