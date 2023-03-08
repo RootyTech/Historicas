a2370ff08d8818357f4c9efe07c99837b7cff779
Daniela Jurado <daniijuradob@gmail.com>
2023-03-08 15:00:44 -0500

    tutorial de instalación

--- components/Welcome/index.jsx
+++ components/Welcome/index.jsx
@@ -1,11 +1,17 @@
 import styles from "../../styles/welcome.module.css";
 import {RxDoubleArrowDown} from "react-icons/rx"
+import { useRouter } from 'next/router'
 
 export const Welcome = () => {
   const scrollTop = () => {
     const height = screen.height;
     window.scrollTo({ top: height, behavior: "smooth" });
   };
+  const router = useRouter();
+  const goToInstall = () => {
+    router.push('/install')
+  }
+
 
   return (
     <section className={styles.welcome_container}>
@@ -13,10 +19,12 @@ export const Welcome = () => {
         <img src="./images/logo_white.png" alt="Logo Historicas"/>
       </div>
       <h1>"No somos histéricas, somos históricas"</h1>
-      <p className={styles.install}>Instala esta app y recibe cada día una notificación sobre una mujer histórica 🔔</p>
-      <span>📲</span>
-      <p>Tap en el icono de Compartir sitio web y en ‘Añadir a Inicio’</p>
-      <p className={styles.ux}>👉🏾Para una mejor experiencia usa tu móvil 👈🏾</p>
+      <div>
+        <p className={styles.install}>Instala esta app y recibe cada día una notificación sobre una mujer histórica 🔔</p>
+        <br />
+        <p className={styles.ux_install} onClick={goToInstall}>¿Cómo instalar Históricas?</p>
+      </div>
+      <span className={styles.ux}>👉🏾Para una mejor experiencia usa tu móvil 👈🏾</span>
       <div className={styles.btn} onClick={scrollTop}>
         <RxDoubleArrowDown />
         <p>La Histórica del Día</p>
---
+++ pages/install.js
@@ -0,0 +1,39 @@
+import stylesInstall from "../styles/install.module.css";
+import styles from '../styles/form.module.css'
+import { useRouter } from 'next/router'
+import { Footer } from "@/components/Footer";
+import Head from 'next/head'
+import {BsAndroid, BsApple} from 'react-icons/bs'
+export default function Install() {
+  const router = useRouter();
+  const goToInstall = () => {
+    router.push('/install')
+  }
+  const goToHome = () => {
+    router.push('/')
+  }
+
+  return (
+    <>
+        <Head>
+            <title>Históricas | Instalación</title>
+            <meta name="description" content="Compártele al mundo esa mujer que inspira y lucha con nosotras día a día" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            {/* <link rel="icon" href="/favicon.ico" /> */}
+        </Head>
+        <main className={stylesInstall.container}>
+            <div className={styles.logo}>
+                <img src='./images/logo_black.png' alt='Logo historicas' onClick={goToHome}/>
+            </div>
+            <h1>Guia de instalación</h1>
+            
+            <p><BsAndroid /> Android</p>
+            <video src="./images/TutorialAndroid.mp4" autoPlay loop width="250">Video Tutorial Android</video>
+            
+            <p><BsApple />IOS</p>
+            <video src="./images/TutorialiOS.mp4" autoPlay loop width="250">Video Tutorial IOS</video>
+        </main>
+        <Footer />
+    </>
+  );
+};
